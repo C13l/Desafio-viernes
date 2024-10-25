ae8002f5b6c99c102081c084eb1f94f5a34c8ba7
Ciel <Ciel@gmail.com>
2024-10-25 19:06:59 -0300

    Desafío 7 - Commit

---
+++ Desafíos/Desafío7/des7.js
@@ -0,0 +1,8 @@
+let precio = document.getElementById("precioProducto");
+precio.innerText = "Precio: $450";
+
+let h1 = document.querySelector("#nombreProducto");
+h1.style.color = "blue";
+h1.style.fontFamily = "Georgia, 'Times New Roman', Times, serif";
+h1.style.margin = "30px";
+h1.style.textAlign = "center";
